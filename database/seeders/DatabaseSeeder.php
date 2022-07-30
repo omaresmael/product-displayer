c1e5914344c544f1fd17a3810e7092adfcf571c2
omaresmael <omaresmael590@gmail.com>
2022-07-30 17:23:00 +0200

    migrate to laravel 9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,63 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $data = $this->loadFromJson();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach ($data['categories'] as $category) {
+            $this->storeCategory($category);
+        }
+    }
+
+    public function loadFromJson()
+    {
+        $json = file_get_contents(__DIR__ . '/sources/products_and_categories.json');
+        $array = json_decode($json, true, 512, JSON_OBJECT_AS_ARRAY);
+        return $array;
+    }
+
+    public function storeCategory($data, $parentId = null)
+    {
+        $name = $data['name'];
+        $subcategories = !empty($data['categories']) ? $data['categories'] : [];
+        $products = !empty($data['products']) ? $data['products'] : [];
+
+        $now = Carbon::now();
+
+        $categoryId = DB::table('categories')->insertGetId([
+            'name' => $name,
+            'parent_id' => $parentId,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+
+        if (!empty($subcategories)) {
+            foreach ($subcategories as $subcategory) {
+                $this->storeCategory($subcategory, $categoryId);
+            }
+        }
+
+        if (!empty($products)) {
+            foreach ($products as $product) {
+                $this->storeProduct($product, $categoryId);
+            }
+        }
+    }
+
+    public function storeProduct($data, $categoryId = null)
+    {
+        $name = $data['name'];
+        $price = !empty($data['price']) ? $data['price'] : 0;
+        $description = !empty($data['description']) ? $data['description'] : '';
+
+        $now = Carbon::now();
+
+        DB::table('products')->insert([
+            'name' => $name,
+            'price' => $price,
+            'description' => $description,
+            'category_id' => $categoryId,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
     }
 }
