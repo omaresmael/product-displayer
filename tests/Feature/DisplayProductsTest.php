6d3cfcddf3d223aff73ac51953c28c396137a8d1
omaresmael <omaresmael590@gmail.com>
2022-07-31 17:35:31 +0200

    prepare render tests

---
+++ tests/Feature/DisplayProductsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+it('renders all the products when visiting the page',function(){
+
+});
+
+
+it('renders all the products of the parent category',function(){
+
+});
+
+it('renders all the products of the subcategory',function(){
+
+});
+
